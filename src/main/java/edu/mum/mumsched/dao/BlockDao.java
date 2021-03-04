c338d83f76a57d38e3f7634f7c65074012fd30c9
Vu Pham <33835853+vp125@users.noreply.github.com>
2021-03-04 17:05:17 -0600

    - Implement Entry and Block

---
+++ src/main/java/edu/mum/mumsched/dao/BlockDao.java
@@ -0,0 +1,21 @@
+package edu.mum.mumsched.dao;
+
+import edu.mum.mumsched.domain.Block;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlockDao extends CrudRepository<Block, Long> {
+    @Query("select s from Block s where s.blockID= :blockID")
+    public Block findBlockByBlockID(@Param("blockID") int blockID);
+
+    @Query("select s from Block s where s.blockName= :blockName")
+    public Block findBlockByBlockName(@Param("blockName") String blockName);
+
+    @Query("select s from Block s")
+    public List<Block> getAllBlock();
+}
---
+++ src/main/java/edu/mum/mumsched/dao/EntryDao.java
@@ -0,0 +1,21 @@
+package edu.mum.mumsched.dao;
+
+import edu.mum.mumsched.domain.Entry;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EntryDao extends CrudRepository<Entry, Long> {
+    @Query("select s from Entry s where s.entryID= :entryID")
+    public Entry findEntryByEntryID(@Param("entryID") int entryID);
+
+    @Query("select s from Entry s where s.entryName= :entryName")
+    public Entry findEntryByEntryName(@Param("entryName") String entryName);
+
+    @Query("select s from Entry s")
+    public List<Entry> getAllEntry();
+}
