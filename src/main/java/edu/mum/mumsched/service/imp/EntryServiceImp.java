9d99c47dd3bc7a3a8489a9643135c6c0a39fc4f6
Vu Pham <33835853+vp125@users.noreply.github.com>
2021-03-04 18:00:58 -0600

    - Update Entry and Block CSS

---
+++ src/main/java/edu/mum/mumsched/service/imp/EntryServiceImp.java
@@ -0,0 +1,35 @@
+package edu.mum.mumsched.service.imp;
+
+import edu.mum.mumsched.dao.EntryDao;
+import edu.mum.mumsched.domain.Entry;
+import edu.mum.mumsched.service.EntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EntryServiceImp implements EntryService {
+    @Autowired
+    EntryDao entryDAO;
+
+    public void save(Entry entry){
+        entryDAO.save(entry);
+        return;
+    }
+
+    @Override
+    public Entry getEntryByEntryName(String entryName) {
+        return entryDAO.findEntryByEntryName(entryName);
+    }
+
+    @Override
+    public Entry getEntryByEntryID(int entryID){
+        return entryDAO.findEntryByEntryID(entryID);
+    }
+
+    @Override
+    public List<Entry> getAllEntry() {
+        return entryDAO.getAllEntry();
+    }
+}
